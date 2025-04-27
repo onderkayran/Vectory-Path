9f1a903f35613d1af437679e5a85e165c5e5b0c6
onderkayran <onderkayran@gmail.com>
2025-04-27 15:05:29 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,9 @@
-
+ const firebaseConfig = {
+    apiKey: "AIzaSyB4lQ2KZanAK3fzRmwXhedh4rtfb6PZ9FY",
+    authDomain: "vectory-path.firebaseapp.com",
+    databaseURL: "https://vectory-path-default-rtdb.firebaseio.com",
+    projectId: "vectory-path",
+    storageBucket: "vectory-path.firebasestorage.app",
+    messagingSenderId: "462766360594",
+    appId: "1:462766360594:web:9a9443f056d45155c8b0cb",
+    measurementId: "G-KCKK8ZRHTT"
